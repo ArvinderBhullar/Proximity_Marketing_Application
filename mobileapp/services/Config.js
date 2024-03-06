17906c690cfcbd9e46b2d64924d4979474a9b3f5
ArvinderBhullar <114533843+ArvinderBhullar@users.noreply.github.com>
2024-03-06 02:07:40 -0700

    Changes to make iOS version work

--- mobileapp/services/Config.js
+++ mobileapp/services/Config.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Initialize firestore
 const db = getFirestore(app);
