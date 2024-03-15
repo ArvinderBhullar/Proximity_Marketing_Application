5ffc5c0ade77af045fe7a4c06ebaa03051864694
Anuj Pradeep <pradeepanuj123@gmail.com>
2024-03-14 21:44:41 -0600

    Implemented Bleutooth scanning, will move the actual implementation to the right screen once its made

--- mobileapp/screens/HomeScreen.js
+++ mobileapp/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   ScrollView,
   Text,
@@ -10,12 +10,42 @@ import {useNavigation} from '@react-navigation/native';
 import styles from '../cssStyles/styles';
 import {auth} from '../services/Config';
 import {getAuth, signOut, onAuthStateChanged} from 'firebase/auth';
+import useBLE from './../services/BLEScan';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const {requestPermissions, scanForDevices, allDevices} = useBLE();
   // const auth = getAuth();
   var displayName = '';
   var uid = '';
+
+  // Functionality to scan for new devices every 10 seconds
+  useEffect(() => {
+    const fetchDevicesInterval = setInterval(() => {
+      fetchAllDevices();
+    }, 10000); // Fetch devices every 10 seconds
+
+    return () => clearInterval(fetchDevicesInterval);
+  }, []);
+
+  // Function to scan for devices if permission was granted by the user to do so
+  const fetchAllDevices = () => {
+    requestPermissions(isGranted => {
+      console.log(isGranted ? 'Granted' : 'Not Granted');
+      if (isGranted) {
+        scanForDevices();
+      }
+    });
+  };
+
+  // DEBUG: simple code just to print the name and RSSI of the device
+  const printAllDevices = () => {
+    allDevices.forEach(device => {
+      console.log(
+        `Device Name: ${device['localName']}, RSSI: ${device['rssi']}`,
+      );
+    });
+  };
 
   onAuthStateChanged(auth, user => {
     if (user) {
@@ -28,6 +58,7 @@ const HomeScreen = () => {
       // User is signed out
     }
   });
+
   const handleLogout = async () => {
     // const auth = getAuth();
     signOut(auth)
@@ -50,6 +81,10 @@ const HomeScreen = () => {
       </View>
       <TouchableOpacity style={styles.button} onPress={handleLogout}>
         <Text style={styles.button}> Logout </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={printAllDevices}>
+        {/* Can remove the button once we dont need to print the devices */}
+        <Text style={styles.button}> Print devices </Text>
       </TouchableOpacity>
     </ScrollView>
   );
