4aa81360467dd6bf5125f70d202e1d13be578f71
Anuj Pradeep <pradeepanuj123@gmail.com>
2024-04-10 14:26:56 -0600

    Allow toggling of the simulation

--- mobileapp/screens/Profile.tsx
+++ mobileapp/screens/Profile.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {signOut} from 'firebase/auth';
 import {auth} from '../services/Config';
 import {Button} from 'react-native-paper';
+import {toggleSimulation} from './Coupons';
 
 const Profile = () => {
+  const [buttonText, setButtonText] = useState('Begin');
+
   const handleLogout = async () => {
     // const auth = getAuth();
     signOut(auth)
@@ -21,7 +24,17 @@ const Profile = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Profile Screen</Text>
-      <Button testID="logoutButton" onPress={handleLogout}>Logout</Button>
+      <Button testID="logoutButton" onPress={handleLogout}>
+        Logout
+      </Button>
+      <Button
+        testID="Simulation"
+        onPress={() => {
+          toggleSimulation();
+          setButtonText(prevState => (prevState === 'Begin' ? 'End' : 'Begin'));
+        }}>
+        {buttonText} Simulation
+      </Button>
     </View>
   );
 };
